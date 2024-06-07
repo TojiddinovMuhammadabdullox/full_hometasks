d198979fa745c16807e277316d58108b87de429a
TojiddinovMuhammadabdullox <rahmatillooyatillo9@gmail.com>
2024-06-07 13:39:29 +0500

    sasaas

--- lib/views/screens/notes_screen.dart
+++ lib/views/screens/notes_screen.dart
@@ -1,9 +1,11 @@
+// ignore_for_file: use_build_context_synchronously, library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class NotesScreen extends StatefulWidget {
-  const NotesScreen({Key? key}) : super(key: key);
+  const NotesScreen({super.key});
 
   @override
   _NotesScreenState createState() => _NotesScreenState();
