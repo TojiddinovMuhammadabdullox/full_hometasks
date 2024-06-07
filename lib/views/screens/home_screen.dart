f330f62995fc7df524781ff7976be03cecae8293
TojiddinovMuhammadabdullox <rahmatillooyatillo9@gmail.com>
2024-06-07 13:22:17 +0500

    kasas

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
--- lib/views/screens/notes_screen.dart
+++ lib/views/screens/notes_screen.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 class NotesScreen extends StatefulWidget {
-  const NotesScreen({super.key});
+  const NotesScreen({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _NotesScreenState createState() => _NotesScreenState();
 }
 
 class _NotesScreenState extends State<NotesScreen> {
-  final List<String> _notes = [];
+  late Database _database;
+  final TextEditingController _textEditingController = TextEditingController();
+  final List<Map<String, dynamic>> _notes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeDatabase();
+  }
+
+  Future<void> _initializeDatabase() async {
+    _database = await openDatabase(
+      join(await getDatabasesPath(), 'notes_database.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE notes(id INTEGER PRIMARY KEY, content TEXT)',
+        );
+      },
+      version: 1,
+    );
+    _fetchNotes();
+  }
+
+  Future<void> _fetchNotes() async {
+    final List<Map<String, dynamic>> notes = await _database.query('notes');
+    setState(() {
+      _notes.clear();
+      _notes.addAll(notes);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,48 +53,55 @@ class _NotesScreenState extends State<NotesScreen> {
         itemBuilder: (context, index) {
           final note = _notes[index];
           return ListTile(
-            title: Text(note),
+            title: Text(note['content']),
             trailing: IconButton(
               icon: const Icon(Icons.delete),
               onPressed: () {
-                _deleteNoteDialog(index);
+                _deleteNoteDialog(context, note['id']);
               },
             ),
             onTap: () {
-              _editNoteDialog(note, index);
+              _editNoteDialog(context, note);
             },
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _addNoteDialog,
+        onPressed: () {
+          _addNoteDialog(context);
+        },
         child: const Icon(Icons.add),
       ),
     );
   }
 
-  void _addNoteDialog() {
-    String note = '';
-    showDialog(
+  Future<void> _addNoteDialog(BuildContext context) async {
+    _textEditingController.clear();
+    await showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
           title: const Text('Add Note'),
           content: TextField(
-            onChanged: (value) {
-              note = value;
-            },
+            controller: _textEditingController,
+            autofocus:
+                true, // Ensure text field gets focus when dialog is shown
             decoration: const InputDecoration(hintText: "Note content"),
           ),
           actions: [
             TextButton(
               onPressed: () {
-                if (note.isNotEmpty) {
-                  setState(() {
-                    _notes.add(note);
-                  });
-                }
                 Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () async {
+                final content = _textEditingController.text;
+                if (content.isNotEmpty) {
+                  await _insertNote(content);
+                  Navigator.of(context).pop();
+                }
               },
               child: const Text('Add'),
             ),
@@ -74,26 +111,41 @@ class _NotesScreenState extends State<NotesScreen> {
     );
   }
 
-  void _editNoteDialog(String oldNote, int index) {
-    String note = oldNote;
-    showDialog(
+  Future<void> _insertNote(String content) async {
+    await _database.insert(
+      'notes',
+      {'content': content},
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+    _fetchNotes();
+  }
+
+  Future<void> _editNoteDialog(
+      BuildContext context, Map<String, dynamic> note) async {
+    final TextEditingController controller =
+        TextEditingController(text: note['content']);
+    await showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
           title: const Text('Edit Note'),
           content: TextField(
-            controller: TextEditingController(text: oldNote),
-            onChanged: (value) {
-              note = value;
-            },
+            controller: controller,
+            autofocus:
+                true, // Ensure text field gets focus when dialog is shown
             decoration: const InputDecoration(hintText: "Note content"),
           ),
           actions: [
             TextButton(
               onPressed: () {
-                setState(() {
-                  _notes[index] = note;
-                });
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () async {
+                final content = controller.text;
+                await _updateNote(note['id'], content);
                 Navigator.of(context).pop();
               },
               child: const Text('Save'),
@@ -104,26 +156,34 @@ class _NotesScreenState extends State<NotesScreen> {
     );
   }
 
-  void _deleteNoteDialog(int index) {
-    showDialog(
+  Future<void> _updateNote(int id, String content) async {
+    await _database.update(
+      'notes',
+      {'id': id, 'content': content},
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    _fetchNotes();
+  }
+
+  Future<void> _deleteNoteDialog(BuildContext context, int id) async {
+    await showDialog(
       context: context,
-      builder: (context) {
+      builder: (BuildContext dialogContext) {
         return AlertDialog(
           title: const Text('Delete Note'),
           content: const Text('Are you sure you want to delete this note?'),
           actions: [
             TextButton(
-              onPressed: () {
-                setState(() {
-                  _notes.removeAt(index);
-                });
-                Navigator.of(context).pop();
+              onPressed: () async {
+                await _deleteNote(id);
+                Navigator.of(dialogContext).pop();
               },
               child: const Text('Delete'),
             ),
             TextButton(
               onPressed: () {
-                Navigator.of(context).pop();
+                Navigator.of(dialogContext).pop();
               },
               child: const Text('Cancel'),
             ),
@@ -131,5 +191,20 @@ class _NotesScreenState extends State<NotesScreen> {
         );
       },
     );
+  }
+
+  Future<void> _deleteNote(int id) async {
+    await _database.delete(
+      'notes',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    _fetchNotes();
+  }
+
+  @override
+  void dispose() {
+    _database.close();
+    super.dispose();
   }
 }
